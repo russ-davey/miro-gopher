28ac24a1928ee984ebd7fbde49aa43f3651b6b9d
russ-davey <russell.j.davey@gmail.com>
2023-04-23 17:37:00 +0100

    tidy-up

--- miro/board_members.go
+++ miro/board_members.go
@@ -7,12 +7,3 @@ type BoardMembersService struct {
 type ShareBoardRequest struct {
 	Emails []string `json:"emails"`
 }
-
-//func (b *BoardMembersService) Share(id string, request *ShareBoardRequest) (*ListBoardsResponse, error) {
-//	response := &ListBoardsResponse{}
-//
-//	url := fmt.Sprintf("%s/boards/%s/members", b.client.BaseURL, id)
-//	err := b.client.Post(url, request, response)
-//
-//	return response, err
-//}
