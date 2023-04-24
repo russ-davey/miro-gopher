306c0ada5dec1da4d2be79e12bb2bbf141f56fe7
russ-davey <russell.j.davey@gmail.com>
2023-04-24 16:27:23 +0100

    add omitempty tags to policy structs so that they can be optional when creating a board

--- miro/boards_responses.go
+++ miro/boards_responses.go
@@ -78,18 +78,19 @@ type ListBoardsLinks struct {
 type SharingPolicy struct {
 	// Access Defines the public-level access to the board.
 	// Valid options: private | view | edit | comment
-	Access string `json:"access"`
+	Access string `json:"access,omitempty"`
 	// InviteToAccountAndBoardLinkAccess Defines the user role when inviting a user via the invite to team and board link.
 	// For Enterprise users, this parameter is always set to no_access regardless of the value that you assign for this parameter.
 	// Valid options: viewer | commenter | editor | no_access
-	InviteToAccountAndBoardLinkAccess string `json:"inviteToAccountAndBoardLinkAccess"`
+	InviteToAccountAndBoardLinkAccess string `json:"inviteToAccountAndBoardLinkAccess,omitempty"`
 	// OrganizationAccess Defines the organization-level access to the board. If the board is created for a team that does
 	// not belong to an organization, the organizationAccess parameter is always set to the default value.
+	// Warning: may result in a "One of the requested features is not supported. (4.0408)" error message if you don't have the necessary access level.
 	// Valid options: private | view | edit | comment
-	OrganizationAccess string `json:"organizationAccess"`
+	OrganizationAccess string `json:"organizationAccess,omitempty"`
 	// TeamAccess Defines the team-level access to the board.
 	// Valid options: private | view | edit | comment
-	TeamAccess string `json:"teamAccess"`
+	TeamAccess string `json:"teamAccess,omitempty"`
 }
 
 // PermissionsPolicy Defines the permissions policies for the board.
@@ -97,25 +98,25 @@ type PermissionsPolicy struct {
 	// CollaborationToolsStartAccess Defines who can start or stop timer, voting, video chat, screen sharing, attention management.
 	// Others will only be able to join. To change the value for the collaborationToolsStartAccess parameter, contact Miro Customer Support.
 	// Valid options: all_editors | board_owners_and_coowners
-	CollaborationToolsStartAccess string `json:"collaborationToolsStartAccess"`
+	CollaborationToolsStartAccess string `json:"collaborationToolsStartAccess,omitempty"`
 	// CopyAccess Defines who can copy the board, copy objects, download images, and save the board as a template or PDF.
 	// Valid options: anyone | team_members | team_editors | board_owner
-	CopyAccess string `json:"copyAccess"`
+	CopyAccess string `json:"copyAccess,omitempty"`
 	// CopyAccessLevel ...
-	CopyAccessLevel string `json:"copyAccessLevel"`
+	CopyAccessLevel string `json:"copyAccessLevel,omitempty"`
 	// SharingAccess Defines who can change access and invite users to this board. To change the value for the sharingAccess
 	// parameter, contact Miro Customer Support.
 	// Valid options: team_members_with_editing_rights | board_owners_and_coowners
-	SharingAccess string `json:"sharingAccess"`
+	SharingAccess string `json:"sharingAccess,omitempty"`
 }
 
 type Policy struct {
 	// PermissionsPolicy Defines the permissions policies for the board.
-	PermissionsPolicy `json:"permissionsPolicy"`
+	PermissionsPolicy `json:"permissionsPolicy,omitempty"`
 	// SharingPolicy Defines the public-level, organization-level, and team-level access for the board. The access level
 	// that a user gets depends on the highest level of access that results from considering the public-level, team-level,
 	// organization-level, and direct sharing access.
-	SharingPolicy `json:"sharingPolicy"`
+	SharingPolicy `json:"sharingPolicy,omitempty"`
 }
 
 type Project struct {
