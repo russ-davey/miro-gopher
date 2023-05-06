363f8f9f511e3eb8ebe9995bfe2b914d3cb3d14b
russ-davey <russell.j.davey@gmail.com>
2023-05-06 12:23:09 +0100

    rename receivers

--- miro/text_items.go
+++ miro/text_items.go
@@ -9,49 +9,49 @@ type TextItemsService struct {
 
 // Create Adds a text item to a board.
 // Required scope: boards:write | Rate limiting: Level 2
-func (c *TextItemsService) Create(boardID string, payload TextItemSet) (*TextItem, error) {
+func (t *TextItemsService) Create(boardID string, payload TextItemSet) (*TextItem, error) {
 	response := &TextItem{}
 
-	if url, err := constructURL(c.client.BaseURL, c.apiVersion, c.resource, boardID, c.subResource); err != nil {
+	if url, err := constructURL(t.client.BaseURL, t.apiVersion, t.resource, boardID, t.subResource); err != nil {
 		return response, err
 	} else {
-		err = c.client.Post(c.client.ctx, url, payload, response)
+		err = t.client.Post(t.client.ctx, url, payload, response)
 		return response, err
 	}
 }
 
 // Get Retrieves information for a specific text item on a board
 // Required scope: boards:read | Rate limiting: Level 1
-func (c *TextItemsService) Get(boardID, itemID string) (*TextItem, error) {
+func (t *TextItemsService) Get(boardID, itemID string) (*TextItem, error) {
 	response := &TextItem{}
 
-	if url, err := constructURL(c.client.BaseURL, c.apiVersion, c.resource, boardID, c.subResource, itemID); err != nil {
+	if url, err := constructURL(t.client.BaseURL, t.apiVersion, t.resource, boardID, t.subResource, itemID); err != nil {
 		return response, err
 	} else {
-		err = c.client.Get(c.client.ctx, url, response)
+		err = t.client.Get(t.client.ctx, url, response)
 		return response, err
 	}
 }
 
 // Update a text item on a board based on the data and style properties provided in the request body.
 // Required scope: boards:write | Rate limiting: Level 2
-func (c *TextItemsService) Update(boardID, itemID string, payload TextItemSet) (*TextItem, error) {
+func (t *TextItemsService) Update(boardID, itemID string, payload TextItemSet) (*TextItem, error) {
 	response := &TextItem{}
 
-	if url, err := constructURL(c.client.BaseURL, c.apiVersion, c.resource, boardID, c.subResource, itemID); err != nil {
+	if url, err := constructURL(t.client.BaseURL, t.apiVersion, t.resource, boardID, t.subResource, itemID); err != nil {
 		return response, err
 	} else {
-		err = c.client.Patch(c.client.ctx, url, payload, response)
+		err = t.client.Patch(t.client.ctx, url, payload, response)
 		return response, err
 	}
 }
 
 // Delete a text item from the board.
 // Required scope: boards:write | Rate limiting: Level 3
-func (c *TextItemsService) Delete(boardID, itemID string) error {
-	if url, err := constructURL(c.client.BaseURL, c.apiVersion, c.resource, boardID, c.subResource, itemID); err != nil {
+func (t *TextItemsService) Delete(boardID, itemID string) error {
+	if url, err := constructURL(t.client.BaseURL, t.apiVersion, t.resource, boardID, t.subResource, itemID); err != nil {
 		return err
 	} else {
-		return c.client.Delete(c.client.ctx, url)
+		return t.client.Delete(t.client.ctx, url)
 	}
 }
